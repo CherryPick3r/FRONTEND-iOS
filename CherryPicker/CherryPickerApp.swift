3e5fd68d51309ae2cfe1d2fa9803c5c0eeeebf8a
ShapeKim98 <talkers.potters-0p@icloud.com>
2023-04-04 13:45:59 +0900

    init Project

---
+++ CherryPicker/CherryPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CherryPickerApp.swift
+//  CherryPicker
+//
+//  Created by 김도형 on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CherryPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
